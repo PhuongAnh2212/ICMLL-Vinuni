7b20b6a89422b551174c1a5676f7984a2cc23070
Phuong Anh <pdpa.work@gmail.com>
2026-09-20 22:25:46 +0700

    Update gsap.ts

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -5,3 +5,4 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 if (typeof window !== "undefined") gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 export { gsap, ScrollTrigger };
+if (typeof window !== "undefined") (window as unknown as { __gsap: typeof gsap }).__gsap = gsap; // TEMP-DEBUG
